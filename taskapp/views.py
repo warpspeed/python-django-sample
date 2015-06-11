e051f3ac24d36bcdd7d1c7206d75bf7b2b3905d0
Chris Turner <chris@turnerlogic.com>
2015-06-11 13:23:28 -0500

    Request structure tweaks.

--- taskapp/views.py
+++ taskapp/views.py
@@ -5,19 +5,19 @@ from .models import Task
 
 
 def index(request):
-    if request.META.get('REQUEST_METHOD') == 'GET':
+    if (request.POST):
+        task_name = request.POST["name"]
+        if (task_name != ""):
+            task = Task(name=task_name)
+            task.save()
+        return redirect('index')
+    else:
         tasks = Task.objects.all()
         template = loader.get_template('taskapp/index.html')
         context = RequestContext(request, {
             'tasks': tasks,
         })
         return HttpResponse(template.render(context))
-    else:
-        task_name = request.POST["name"]
-        if (task_name != ""):
-            task = Task(name=task_name)
-            task.save()
-        return redirect('index')
 
 
 def clear_complete(request):
@@ -29,7 +29,8 @@ def clear_complete(request):
 
 
 def toggle_complete(request, task_id):
-    task = Task.objects.get(id=task_id)
-    task.toggle_complete()
-    task.save()
+    if (request.POST):
+        task = Task.objects.get(id=task_id)
+        task.toggle_complete()
+        task.save()
     return redirect('index')

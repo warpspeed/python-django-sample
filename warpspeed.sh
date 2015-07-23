aa7680a801b371f885957902d487030111ec1dee
zachconner <zattack924@gmail.com>
2015-07-23 01:20:06 -0500

    Added warpspeed.sh

---
+++ warpspeed.sh
@@ -0,0 +1,18 @@
+# warpspeed.sh
+# Commands here will be run each time a pull or push deploy is successfully run.
+
+# Activate your virtualenv.
+source env/bin/activate
+
+# Install dependencies.
+pip install -r requirements.txt
+
+# Run the database migrations.
+python manage.py migrate
+
+# Restart passenger.
+touch tmp/restart.txt
+
+# Deactivate the virtualenv.
+deactivate
+
